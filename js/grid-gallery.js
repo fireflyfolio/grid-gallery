2d5987f8d96e640df58ba1c0e75a9d224f932512
FireflyFolio <6480990+fireflyfolio@users.noreply.github.com>
2024-06-15 00:34:43 +0200

    Improve images navigation

--- js/grid-gallery.js
+++ js/grid-gallery.js
@@ -2,71 +2,93 @@ const root = document.querySelector("body, html");
 const container = document.querySelector('.gg-container');
 const images = document.querySelectorAll(".gg-box > img");
 const l = images.length;
+let prevImg, nextImg;
 
-for(var i = 0; i < l; i++) {
-  images[i].addEventListener("click", function(i) {
-    var currentImg = this;
-    const parentItem = currentImg.parentElement, screenItem = document.createElement('div');
+for (let i = 0; i < l; i++) {
+  images[i].addEventListener("click", function() {
+    let currentImg = this;    
+    const parentItem = currentImg.parentElement;
+    const screenItem = document.createElement('div');
     screenItem.id = "gg-screen";
-    container.prepend(screenItem);
-    if (parentItem.hasAttribute('data-theme')) screenItem.setAttribute("data-theme", "dark");
-    var route = currentImg.src;
-    root.style.overflow = 'hidden';
     screenItem.innerHTML = '<div class="gg-image"></div><div class="gg-close gg-btn">&times</div><div class="gg-next gg-btn">&rarr;</div><div class="gg-prev gg-btn">&larr;</div>';
-    const first = images[0].src, last = images[l-1].src;
-    const imgItem = document.querySelector(".gg-image"), prevBtn = document.querySelector(".gg-prev"), nextBtn = document.querySelector(".gg-next"), close = document.querySelector(".gg-close");
+
+    container.prepend(screenItem);
+
+    if (parentItem.hasAttribute('data-theme')) 
+      screenItem.setAttribute("data-theme", "dark");
+
+    let route = currentImg.src;
+    root.style.overflow = 'hidden';
+
+    const first = images[0].src;
+    const last = images[l-1].src;
+    const imgItem = document.querySelector(".gg-image");
+    const prevBtn = document.querySelector(".gg-prev");
+    const nextBtn = document.querySelector(".gg-next");
+    const close = document.querySelector(".gg-close");
+
     imgItem.innerHTML = '<img src="' + route + '">';
+    imgItem.addEventListener("click", next);
 
     if (l > 1) {
-      if (route == first) {
+      if (route === first) {
         prevBtn.hidden = true;
-        var prevImg = false;
-        var nextImg = currentImg.nextElementSibling;
-      }
-      else if (route == last) {
+        prevImg = false;
+        nextImg = currentImg.nextElementSibling;
+      } else if (route === last) {
         nextBtn.hidden = true;
-        var nextImg = false;
-        var prevImg = currentImg.previousElementSibling;
+        nextImg = false;
+        prevImg = currentImg.previousElementSibling;
+      } else {
+        prevImg = currentImg.previousElementSibling;
+        nextImg = currentImg.nextElementSibling;
       }
-      else {
-        var prevImg = currentImg.previousElementSibling;
-        var nextImg = currentImg.nextElementSibling;
-      }
-    }
-    else {
+    } else {
       prevBtn.hidden = true;
       nextBtn.hidden = true;
     }
 
     screenItem.addEventListener("click", function(e) {
-      if (e.target == this || e.target == close) hide();
+      if (e.target === this || e.target === close) hide();
     });
 
     root.addEventListener("keydown", function(e) {
-      if (e.keyCode == 37 || e.keyCode == 38) prev();
-      if (e.keyCode == 39 || e.keyCode == 40) next();
-      if (e.keyCode == 27 ) hide();
+      if (e.keyCode === 37 || e.keyCode === 38) prev();
+      if (e.keyCode === 39 || e.keyCode === 40) next();
+      if (e.keyCode === 27 ) hide();
     });
 
     prevBtn.addEventListener("click", prev);
     nextBtn.addEventListener("click", next);
 
     function prev() {
-      prevImg = currentImg.previousElementSibling;
-      imgItem.innerHTML = '<img src="' + prevImg.src + '">';
-      currentImg = currentImg.previousElementSibling;
-      var mainImg = document.querySelector(".gg-image > img").src;
-      nextBtn.hidden = false;
-      prevBtn.hidden = mainImg === first;
+      try {
+        prevImg = currentImg.previousElementSibling;
+        imgItem.innerHTML = '<img src="' + prevImg.src + '">';
+        currentImg = currentImg.previousElementSibling;
+        let mainImg = document.querySelector(".gg-image > img").src;
+        nextBtn.hidden = false;
+        prevBtn.hidden = mainImg === first;
+      } catch (e) {
+        prevImg = currentImg.parentElement.lastElementChild;
+        imgItem.innerHTML = '<img src="' + prevImg.src + '">';
+        currentImg = prevImg;
+      }
     };
 
     function next() {
-      nextImg = currentImg.nextElementSibling;
-      imgItem.innerHTML = '<img src="' + nextImg.src + '">';
-      currentImg = currentImg.nextElementSibling;
-      var mainImg = document.querySelector(".gg-image > img").src;
-      prevBtn.hidden = false;
-      nextBtn.hidden = mainImg === last;
+      try {
+        nextImg = currentImg.nextElementSibling;
+        imgItem.innerHTML = '<img src="' + nextImg.src + '">';
+        currentImg = currentImg.nextElementSibling;
+        let mainImg = document.querySelector(".gg-image > img").src;
+        prevBtn.hidden = false;
+        nextBtn.hidden = mainImg === last;
+      } catch (e) {
+        nextImg = currentImg.parentElement.firstElementChild;
+        imgItem.innerHTML = '<img src="' + nextImg.src + '">';
+        currentImg = nextImg;
+      }
     };
 
     function hide() {
@@ -77,6 +99,8 @@ for(var i = 0; i < l; i++) {
 }
 
 function gridGallery (options) {
+  let selector;
+
   if (options.selector) selector = document.querySelector(options.selector);
   if (options.darkMode) selector.setAttribute("data-theme", "dark");
   if (options.layout == "horizontal" || options.layout == "square") selector.setAttribute("data-layout", options.layout);
